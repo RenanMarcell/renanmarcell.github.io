3c727ef750f6721b8f71b8530cdc696574bb890c
renan <renanm@outlook.com>
2018-03-26 23:32:16 -0300

    fix js for more validations

--- js/save-info.js
+++ js/save-info.js
@@ -1,11 +1,41 @@
 var $ = document.querySelector.bind(document);
 
-function emailAlreadyUsed() {
-    $('.onfailure').style.display = 'block';
-    $('.onfailure').focus();
-    setTimeout(function(){
-        $('.onfailure').style.display = 'none';
-    }, 2000);
+function showDiv(div, time) {
+    div.style.display = 'block';
+    div.focus();
+    setTimeout(function() {
+        div.style.display = 'none'
+    }, time);
+}
+
+function checkInvalidName(nome) {
+    var $failureDiv = $('.onFailure');
+    if (nome === ''){
+        $failureDiv.innerHTML = "O campo nome é obrigatório";
+        showDiv($failureDiv, 2000);
+        return true;
+    } else if (nome.length < 3) {
+        $failureDiv.innerHTML = "Nome deve ter no minimo 3 caracteres";
+        showDiv($failureDiv, 2000);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkInvalidEmail(email) {
+    var $failureDiv = $('.onFailure');
+    if (savedUsers && email in savedUsers) {
+        $failureDiv.innerHTML = "Email já cadastrado";
+        showDiv($failureDiv, 2000);
+        return true;
+    } else if (email === '' || email.indexOf('.') === -1) {
+        $failureDiv.innerHTML = "Email inválido";
+        showDiv($failureDiv, 2000);
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function registerSuccess() {
@@ -21,15 +51,15 @@ function registerSuccess() {
 function adicionaDados(event) {
     event.preventDefault();
     let nome = $("#nome").value;
+    if (checkInvalidName(nome)) return;
     let email = $("#email").value;
-    if (email in savedUsers) {
-        emailAlreadyUsed();
-        return;
-    }
-    let user = savedUsers;
+    if (checkInvalidEmail(email)) return;
+    let user = savedUsers || {};
     user[email] = nome;
     localStorage.setItem('savedUsers', JSON.stringify(user));
     registerSuccess();
 }
 
-var savedUsers = JSON.parse(localStorage.getItem('savedUsers'));
+if (localStorage.getItem('savedUsers') !== null && localStorage.getItem('savedUsers') !== '') {
+    var savedUsers = JSON.parse(localStorage.getItem('savedUsers'));
+}
